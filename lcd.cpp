0c8c0213c82d55ec07c33db07d564542771135fa
Nico Reist <>
2024-09-18 11:53:47 +0200

    Working upscaled version. Upscale 1.5x

---
+++ lcd.cpp
@@ -0,0 +1,305 @@
+#include "lcd.h"
+#include <Arduino.h>
+#include "cpu.h"
+#include "interrupt.h"
+#include "mem.h"
+#include "sdl.h"
+
+static int lcd_line;
+static int lcd_ly_compare;
+
+/* LCD STAT */
+static int ly_int; /* LYC = LY coincidence interrupt enable */
+static int mode2_oam_int;
+static int mode1_vblank_int;
+static int mode0_hblank_int;
+static int ly_int_flag;
+static int lcd_mode;
+
+/* LCD Control */
+static int lcd_enabled;
+static int window_tilemap_select;
+static int window_enabled;
+static int tilemap_select;
+static int bg_tiledata_select;
+static int sprite_size;
+static int sprites_enabled;
+static int bg_enabled;
+static int scroll_x, scroll_y;
+static int window_x, window_y;
+
+static byte bgpalette[] = {3, 2, 1, 0};
+static byte sprpalette1[] = {0, 1, 2, 3};
+static byte sprpalette2[] = {0, 1, 2, 3};
+
+struct sprite {
+    int y, x, tile, flags;
+};
+
+enum { PRIO = 0x80, VFLIP = 0x40, HFLIP = 0x20, PNUM = 0x10 };
+
+unsigned char lcd_get_stat(void) { 
+    return (ly_int << 6) | lcd_mode; 
+}
+
+void lcd_write_bg_palette(unsigned char n) {
+    bgpalette[0] = (n >> 0) & 3;
+    bgpalette[1] = (n >> 2) & 3;
+    bgpalette[2] = (n >> 4) & 3;
+    bgpalette[3] = (n >> 6) & 3;
+}
+
+void lcd_write_spr_palette1(unsigned char n) {
+    sprpalette1[0] = 0;
+    sprpalette1[1] = (n >> 2) & 3;
+    sprpalette1[2] = (n >> 4) & 3;
+    sprpalette1[3] = (n >> 6) & 3;
+}
+
+void lcd_write_spr_palette2(unsigned char n) {
+    sprpalette2[0] = 0;
+    sprpalette2[1] = (n >> 2) & 3;
+    sprpalette2[2] = (n >> 4) & 3;
+    sprpalette2[3] = (n >> 6) & 3;
+}
+
+void lcd_write_scroll_x(unsigned char n) {
+    scroll_x = n;
+}
+
+void lcd_write_scroll_y(unsigned char n) { 
+    scroll_y = n; 
+}
+
+int lcd_get_line(void) { 
+    return lcd_line; 
+}
+
+void lcd_write_stat(unsigned char c) { 
+    ly_int = !!(c & 0x40); 
+}
+
+void lcd_write_control(unsigned char c) {
+    bg_enabled = !!(c & 0x01);
+    sprites_enabled = !!(c & 0x02);
+    sprite_size = !!(c & 0x04);
+    tilemap_select = !!(c & 0x08);
+    bg_tiledata_select = !!(c & 0x10);
+    window_enabled = !!(c & 0x20);
+    window_tilemap_select = !!(c & 0x40);
+    lcd_enabled = !!(c & 0x80);
+}
+
+void lcd_set_ly_compare(unsigned char c) { 
+    lcd_ly_compare = c; 
+}
+
+void lcd_set_window_y(unsigned char n) { 
+    window_y = n; 
+}
+
+void lcd_set_window_x(unsigned char n) { 
+    window_x = n; 
+}
+
+static void swap(struct sprite *a, struct sprite *b) {
+    struct sprite c;
+    c = *a;
+    *a = *b;
+    *b = c;
+}
+
+static void sort_sprites(struct sprite *s, int n) {
+    int swapped, i;
+    do {
+        swapped = 0;
+        for (i = 0; i < n - 1; i++) {
+            if (s[i].x < s[i + 1].x) {
+                swap(&s[i], &s[i + 1]);
+                swapped = 1;
+            }
+        }
+    } while (swapped);
+}
+
+#define GAMEBOY_HEIGHT 144
+#define GAMEBOY_WIDTH 160
+#define TARGET_HEIGHT 216
+#define TARGET_WIDTH 240
+
+static void draw_bg_and_window(uint8_t *frame_buffer, int line, const unsigned char *raw_mem) {
+    unsigned int map_select, map_offset, tile_num, tile_addr, xm, ym;
+    unsigned char b1, b2, mask, colour;
+
+    if (window_enabled && line >= window_y && (line - window_y) < GAMEBOY_HEIGHT) {
+        xm = 0;
+        ym = line - window_y;
+        map_select = window_tilemap_select;
+    } else {
+        if (!bg_enabled) {
+            // Setze alle Pixel in der aktuellen Zeile auf den Hintergrund
+            for (int x = 0; x < TARGET_WIDTH; ++x) {
+                frame_buffer[line * TARGET_WIDTH + x] = bgpalette[0];
+            }
+            return;
+        }
+        xm = scroll_x % 256;
+        ym = (line + scroll_y) % 256;
+        map_select = tilemap_select;
+    }
+
+    // Neue Skalierung und füllen des gesamten Framebuffers
+    for (int x = 0; x < GAMEBOY_WIDTH; ++x) {
+        map_offset = (ym / 8) * 32 + xm / 8;
+        tile_num = raw_mem[0x9800 + map_select * 0x400 + map_offset];
+        tile_addr = bg_tiledata_select ? 0x8000 + tile_num * 16 : 0x9000 + ((signed char)tile_num) * 16;
+
+        b1 = raw_mem[tile_addr + (ym % 8) * 2];
+        b2 = raw_mem[tile_addr + (ym % 8) * 2 + 1];
+        mask = 128 >> (xm % 8);
+        colour = (!!(b2 & mask) << 1) | !!(b1 & mask);
+
+        int scaledXStart = x * 3 / 2; // 160 -> 240 (1.5x)
+        int scaledXEnd = (x + 1) * 3 / 2; // Berechnung für den nächsten Pixelbereich
+        int scaledLineStart = line * 3 / 2; // 144 -> 216 (1.5x)
+        int scaledLineEnd = (line + 1) * 3 / 2;
+
+        // Fülle den Bereich des skalierenden Pixels im Framebuffer
+        for (int sy = scaledLineStart; sy < scaledLineEnd; ++sy) {
+            for (int sx = scaledXStart; sx < scaledXEnd; ++sx) {
+                int bufferIndex = sy * TARGET_WIDTH + sx;
+                if (bufferIndex >= 0 && bufferIndex < TARGET_WIDTH * TARGET_HEIGHT) {
+                    frame_buffer[bufferIndex] = bgpalette[colour];
+                }
+            }
+        }
+
+        xm = (xm + 1) % 256;
+    }
+}
+
+static void draw_sprites(uint8_t *frame_buffer, int line, int nsprites, struct sprite *s, const unsigned char *raw_mem) {
+    int i;
+
+    for (i = 0; i < nsprites; i++) {
+        unsigned int b1, b2, tile_addr, sprite_line, x;
+
+        if (s[i].x < -7 || s[i].x >= GAMEBOY_WIDTH) continue; // Sprite is outside the screen
+
+        sprite_line = s[i].flags & VFLIP ? (sprite_size ? 15 : 7) - (line - s[i].y) : line - s[i].y;
+
+        tile_addr = 0x8000 + (s[i].tile * 16) + sprite_line * 2;
+
+        b1 = raw_mem[tile_addr];
+        b2 = raw_mem[tile_addr + 1];
+
+        for (x = 0; x < 8; x++) {
+            unsigned char mask, colour;
+            byte *pal;
+
+            if ((s[i].x + x) < 0 || (s[i].x + x) >= GAMEBOY_WIDTH) continue;
+
+            mask = s[i].flags & HFLIP ? 128 >> (7 - x) : 128 >> x;
+            colour = ((!!(b2 & mask)) << 1) | !!(b1 & mask);
+            if (colour == 0) continue;
+
+            pal = (s[i].flags & PNUM) ? sprpalette2 : sprpalette1;
+
+            int scaledXStart = (s[i].x + x) * 3 / 2;
+            int scaledXEnd = (s[i].x + x + 1) * 3 / 2;
+            int scaledLineStart = line * 3 / 2;
+            int scaledLineEnd = (line + 1) * 3 / 2;
+
+            // Setze den Bereich des Pixels im Framebuffer
+            for (int sy = scaledLineStart; sy < scaledLineEnd; ++sy) {
+                for (int sx = scaledXStart; sx < scaledXEnd; ++sx) {
+                    int bufferIndex = sy * TARGET_WIDTH + sx;
+                    if (bufferIndex >= 0 && bufferIndex < TARGET_WIDTH * TARGET_HEIGHT) {
+                        frame_buffer[bufferIndex] = pal[colour];
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+static void render_line(int line) {
+    const unsigned char *raw_mem = mem_get_raw();
+    int i, c = 0;
+
+    struct sprite s[10];
+    uint8_t *buffer = sdl_get_framebuffer();
+
+    for (i = 0; i < 40; i++) {
+        int y = raw_mem[0xFE00 + (i * 4)] - 16;
+        if (line < y || line >= y + 8 + (sprite_size * 8)) continue;
+
+        s[c].y = y;
+        s[c].x = raw_mem[0xFE00 + (i * 4) + 1] - 8;
+        s[c].tile = raw_mem[0xFE00 + (i * 4) + 2];
+        s[c].flags = raw_mem[0xFE00 + (i * 4) + 3];
+        c++;
+
+        if (c == 10) break;
+    }
+
+    if (c) sort_sprites(s, c);
+
+    /* Draw the background layer */
+    draw_bg_and_window(buffer, line, raw_mem);
+
+    draw_sprites(buffer, line, c, s, raw_mem);
+}
+
+#define CYCLES_PER_FRAME (70224 / 4)
+#define CYCLES_PER_LINE (456 / 4)
+
+bool lcd_cycle(unsigned int cycles) {
+    static int this_frame_cycles = 0;
+    static unsigned int prev_cycles = 0;
+    static int sub_line = 0;
+    static int prev_update_cycles = 0;
+
+    this_frame_cycles += cycles - prev_cycles;
+    prev_cycles = cycles;
+
+    if (this_frame_cycles >= CYCLES_PER_FRAME) {
+        this_frame_cycles -= CYCLES_PER_FRAME;
+        prev_update_cycles -= CYCLES_PER_FRAME;
+    }
+
+    if (this_frame_cycles < 456 / 4) {
+        if (this_frame_cycles < 204 / 4)
+            lcd_mode = 2;
+        else if (this_frame_cycles < 284 / 4)
+            lcd_mode = 3;
+        else {
+            lcd_mode = 0;
+            lcd_line = 0;
+        }
+        return false;
+    }
+
+    sub_line += this_frame_cycles - prev_update_cycles;
+    prev_update_cycles = this_frame_cycles;
+
+    if (sub_line >= CYCLES_PER_LINE) {
+        sub_line -= CYCLES_PER_LINE;
+
+        if (lcd_line < GAMEBOY_HEIGHT) render_line(lcd_line);
+
+        lcd_line += 1;
+
+        if (lcd_line >= GAMEBOY_HEIGHT) lcd_mode = 1;
+
+        if (ly_int && lcd_line == lcd_ly_compare) interrupt(INTR_LCDSTAT);
+
+        if (lcd_line == GAMEBOY_HEIGHT) {
+            interrupt(INTR_VBLANK);
+            return true;
+        }
+    }
+    return false;
+}
---
+++ sdl.cpp
@@ -0,0 +1,139 @@
+#include "sdl.h"
+
+#include <Arduino_GFX_Library.h>
+
+#include "SPI.h"
+
+#define _cs 5     // 3 goes to TFT CS
+#define _dc 21    // 4 goes to TFT DC
+#define _mosi 23  // 5 goes to TFT MOSI
+#define _sclk 18  // 6 goes to TFT SCK/CLK
+#define _rst 25   // ESP RST to TFT RESET
+#define _miso 19  // Not connected
+#define _led 22
+//       3.3V     // Goes to TFT LED
+//       5v       // Goes to TFT Vcc
+//       Gnd      // Goes to TFT Gnd
+#define _left GPIO_NUM_4
+#define _right GPIO_NUM_12
+#define _up GPIO_NUM_17
+#define _down GPIO_NUM_16
+#define _select GPIO_NUM_34
+#define _start GPIO_NUM_35
+#define _a GPIO_NUM_39
+#define _b GPIO_NUM_36
+
+Arduino_DataBus *bus = new Arduino_ESP32SPI(_dc);
+// Arduino_DataBus *bus = new Arduino_ESP32SPI(_dc, _cs, _sclk, _mosi, _miso);
+Arduino_GFX *tft = new Arduino_ILI9341(bus, _rst, 3 /* rotation */);
+
+void backlighting(bool state) {
+  if (!state) {
+    digitalWrite(_led, LOW);
+  } else {
+    digitalWrite(_led, HIGH);
+  }
+}
+
+#define GAMEBOY_HEIGHT 144
+#define GAMEBOY_WIDTH 160
+#define DRAW_HEIGHT 216
+#define DRAW_WIDTH 240
+#define SCREEN_HEIGHT 240
+#define SCREEN_WIDTH 320
+
+#define SPI_FREQ 40000000
+
+static uint8_t *frame_buffer;
+
+static int button_start, button_select, button_a, button_b, button_down,
+    button_up, button_left, button_right;
+
+static volatile bool frame_ready = false;
+TaskHandle_t draw_task_handle;
+
+void draw_button(bool value, int x_pos, int y_pos,
+                 const char *label = nullptr) {
+  if (value) {
+    tft->fillCircle(x_pos, y_pos, 7, 0xffff);
+  } else {
+    tft->fillCircle(x_pos, y_pos, 7, 0x0000);
+  }
+  if (label) {
+    int len = strlen(label);
+    constexpr int char_len = 8;
+    int width = char_len * len;
+    for (int i = 0; i < len; ++i) {
+      tft->drawChar(x_pos - width / 2 + char_len * i, y_pos + 20, label[i],
+                    0xffff, BLACK);
+    }
+  }
+}
+
+void draw_task(void *parameter) {
+    uint16_t color_palette[] = {0xffff, (16 << 11) + (32 << 5) + 16, (8 << 11) + (16 << 5) + 8, 0x0000};
+
+    int h_offset = (SCREEN_WIDTH - DRAW_WIDTH) / 2;
+    int v_offset = (SCREEN_HEIGHT - DRAW_HEIGHT) / 2;
+    while (true) {
+        while (!frame_ready) {
+            delay(1);
+        }
+        frame_ready = false;
+        tft->drawIndexedBitmap(h_offset, v_offset, frame_buffer, color_palette, DRAW_WIDTH, DRAW_HEIGHT);
+    }
+}
+
+
+void sdl_init(void) {
+  frame_buffer = new uint8_t[DRAW_WIDTH * DRAW_HEIGHT];
+  // GFX_EXTRA_PRE_INIT();
+  tft->begin(SPI_FREQ);
+  pinMode(_led, OUTPUT);
+  backlighting(true);
+  tft->fillScreen(RED);
+
+  gpio_num_t gpios[] = {_left, _right, _down, _up, _start, _select, _a, _b};
+  for (gpio_num_t pin : gpios) {
+    esp_rom_gpio_pad_select_gpio(pin);
+    gpio_set_direction(pin, GPIO_MODE_INPUT);
+    // uncomment to use builtin pullup resistors
+    //    gpio_set_pull_mode(pin, GPIO_PULLUP_ONLY);
+  }
+  xTaskCreatePinnedToCore(draw_task,  /* Function to implement the task */
+                          "drawTask", /* Name of the task */
+                          10000,      /* Stack size in words */
+                          NULL,       /* Task input parameter */
+                          0,          /* Priority of the task */
+                          &draw_task_handle, /* Task handle. */
+                          0); /* Core where the task should run */
+}
+
+int sdl_update(void) {
+  button_up = !gpio_get_level(_up);
+  button_left = !gpio_get_level(_left);
+  button_down = !gpio_get_level(_down);
+  button_right = !gpio_get_level(_right);
+
+  button_start = !gpio_get_level(_start);
+  button_select = !gpio_get_level(_select);
+
+  button_a = !gpio_get_level(_a);
+  button_b = !gpio_get_level(_b);
+  sdl_frame();
+  return 0;
+}
+
+unsigned int sdl_get_buttons(void) {
+  unsigned int buttons =
+      (button_start * 8) | (button_select * 4) | (button_b * 2) | button_a;
+  return buttons;
+}
+
+unsigned int sdl_get_directions(void) {
+  return (button_down * 8) | (button_up * 4) | (button_left * 2) | button_right;
+}
+
+uint8_t *sdl_get_framebuffer(void) { return frame_buffer; }
+
+void sdl_frame(void) { frame_ready = true; }
